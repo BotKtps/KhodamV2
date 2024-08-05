fdaea0d568aae1de3eac2a68b203d3542c334aca
BotKtps <73116515+BotKtps@users.noreply.github.com>
2024-08-04 17:28:47 -0700

    Add files via upload

--- Khodam.js
+++ Khodam.js
@@ -1,16 +1,16 @@
 document.getElementById('checkButton').addEventListener('click', function() {
     const name = document.getElementById('name').value;
-    const result = document.getElementById('result'); // Perbaikan pada assignment
+    const result = document.getElementById('result');
     if (name) {
         const khodam = checkKhodam(name);
-        result.innerHTML = `Khodam Anda adalah: <strong>${khodam}</strong>`; // Perbaikan pada template literal dan penutupan tag strong
+        result.innerHTML = `Khodam Anda adalah: <strong>${khodam}</strong>`;
     } else {
         result.innerHTML = '<span style="color: red;">Silakan masukkan nama Anda!</span>';
     }
 });
 
 function checkKhodam(name) {
-    const khodams = ['Monyet Kayang', 'Pocong Busuk', 'Kunti Daster', 'Macan Goyang', 'Elang Mabok', 'Tuyul', 'Bebek Penyet', 'Buaya Darat']; // Perbaikan pada penugasan array
+    const khodams = ['Monyet Kayang', 'Pocong Busuk', 'Kunti Daster', 'Macan Goyang', 'Elang Mabok', 'Tuyul', 'Bebek Penyet', 'Buaya Darat', 'Macan Sumatera', 'Kuda Poni', 'Sandal Jepit', 'Zebra Sigma', 'Jembut Skibidi', 'Nekopoi', 'Roket Geprek'];
     const index = Math.floor(Math.random() * khodams.length);
     return khodams[index];
 }
